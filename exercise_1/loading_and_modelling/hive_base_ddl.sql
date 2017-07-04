3ada56c66408e5b045d6554885906c74d2255725
jayzuniga <jay_zuniga@berkeley.edu>
2017-07-03 18:21:59 -0700

    Update hive_base_ddl.sql

--- exercise_1/loading_and_modelling/hive_base_ddl.sql
+++ exercise_1/loading_and_modelling/hive_base_ddl.sql
@@ -1,1 +1,17 @@
-# hello
+CREATE EXTERNAL TABLE measures_raw 
+  (measure_name STRING, 
+   measure_id STRING, 
+   measure_start_quarter STRING, 
+   measure_start_date TIMESTAMP, 
+   measure_end_quarter STRING, 
+   measure_end_date TIMESTAMP)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES (
+ "separatorChar" = ",",
+ "quoteChar" = '"',
+ "escapeChar" = '\\'
+)
+STORED AS TEXTFILE
+LOCATION '/user/w205/hospital_compare/measures';
+
+/user/w205/hospital_compare/Measure
