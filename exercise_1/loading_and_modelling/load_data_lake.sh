cd375c7d2be8b1f7721bb163d429838a964a0c45
jayzuniga <jay_zuniga@berkeley.edu>
2017-07-03 22:05:53 -0700

    Update load_data_lake.sh

--- exercise_1/loading_and_modelling/load_data_lake.sh
+++ exercise_1/loading_and_modelling/load_data_lake.sh
@@ -1,4 +1,17 @@
 # load_data_lake.sh
 
+sudo su - w205
+
 hdfs dfs -mkdir /user/w205/hospital_compare/measures/
+hdfs dfs -mkdir /user/w205/hospital_compare/survey_responses/
+hdfs dfs -mkdir /user/w205/hospital_compare/hospitals/
+hdfs dfs -mkdir /user/w205/hospital_compare/effective_care/
+hdfs dfs -mkdir /user/w205/hospital_compare/readmissions/
+
+cd /user/w205/hospital_compare/
+
 hdfs dfs -put Measures.csv /user/w205/hospital_compare/measures/
+hdfs dfs -put effective_care.csv /user/w205/hospital_compare/effective_care/
+hdfs dfs -put hospitals.csv /user/w205/hospital_compare/hospitals/
+hdfs dfs -put readmissions.csv /user/w205/hospital_compare/readmissions/
+hdfs dfs -put survey_responses.csv /user/w205/hospital_compare/survey_responses/
