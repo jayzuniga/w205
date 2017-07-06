46fad8de184f23a04ec69fcb0efb1b557475d045
jayzuniga <jay_zuniga@berkeley.edu>
2017-07-06 11:16:54 -0700

    Update procedures.sql

--- exercise_1/transforming/procedures.sql
+++ exercise_1/transforming/procedures.sql
@@ -8,13 +8,18 @@ DROP TABLE Procedures;
 CREATE TABLE Procedures
 AS
 SELECT
-  measure_name   AS procedure_name,
-  measure_id     AS procedure_id,
-  min(score)     AS min_score,
-  max(score)     AS max_score,
-  avg(score)     AS avg_score,
-  var(score)     AS var_score
+  measure_name                   AS procedure_name,
+  measure_id                     AS procedure_id,
+  min(cast(score as float))      AS min_score,
+  max(cast(score as float))      AS max_score,
+  avg(cast(score as float))      AS avg_score,
+  variance(cast(score as float)) AS var_score
 FROM effective_care_raw
+WHERE score not IN ("Not Available",
+                    "Low (0 - 19,999 patients annually)",
+                    "Medium (20,000 - 39,999 patients annually)",
+                    "High (40,000 - 59,999 patients annually)",
+                    "Very High (60,000+ patients annually)")
 GROUP BY measure_name,
          measure_id
 ;
