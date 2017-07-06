034cdd13a95f1ea515faf8fe4adc90baef611a14
jayzuniga <jay_zuniga@berkeley.edu>
2017-07-06 13:37:30 -0700

    Update best_hospitals.sql

--- exercise_1/investigations/best_hospitals/best_hospitals.sql
+++ exercise_1/investigations/best_hospitals/best_hospitals.sql
@@ -1,1 +1,11 @@
-#hello
+-- GET TOP 30 Hospitals based on effective and readmissions scores
+SELECT provider_id, 
+       hospital_name, 
+       state_name, 
+       effective_care_avg_score, 
+       readmissions_death_avg_score
+FROM Hospitals
+WHERE readmissions_death_avg_score is NOT NULL
+ORDER BY effective_care_avg_score DESC, 
+         readmissions_death_avg_score ASC
+LIMIT 30;
