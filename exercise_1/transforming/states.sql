a1b77ef1d18c832f6ca31202fd053f5478cadcad
jayzuniga <jay_zuniga@berkeley.edu>
2017-07-06 08:12:19 -0700

    Update states.sql

--- exercise_1/transforming/states.sql
+++ exercise_1/transforming/states.sql
@@ -1,10 +1,14 @@
--- Create States table
+-- This script creates the States table
+
+-- Drop previous instance of the States table if it exists, so that it can
+-- be refreshed with most recent data
 DROP TABLE States;
 
+-- Create States table using effective_care_raw table
 CREATE TABLE States
 AS
 SELECT
-  e.state,
+  e.state                      AS state,
   avg(cast(e.score as float))  AS effective_care_avg_score,
   avg(cast(rd.score as float)) AS readmission_death_avg_score
 FROM effective_care_raw    AS e
