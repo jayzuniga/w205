ea3bf2ece5ea73f0711e1cc9551b5d08e26a9813
jayzuniga <jay_zuniga@berkeley.edu>
2017-07-06 14:22:02 -0700

    Update best_states.sql

--- exercise_1/investigations/best_states/best_states.sql
+++ exercise_1/investigations/best_states/best_states.sql
@@ -1,1 +1,11 @@
-#1
+-- This script gets the top 20 states based on their hospital
+-- averages
+
+SELECT *
+FROM States
+WHERE effective_care_avg_score IS NOT NULL 
+AND readmission_death_avg_score IS NOT NULL
+ORDER BY effective_care_avg_score DESC,
+         readmission_death_avg_score ASC
+LIMIT 20         
+;
