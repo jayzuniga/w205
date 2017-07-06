512e315813ba2abbc0eac896db54fb9a1441cb7e
jayzuniga <jay_zuniga@berkeley.edu>
2017-07-06 16:18:46 -0700

    Update hospital_variability.sql

--- exercise_1/investigations/hospital_variability/hospital_variability.sql
+++ exercise_1/investigations/hospital_variability/hospital_variability.sql
@@ -1,1 +1,9 @@
-#1
+-- This script returns the top 20 hospital procedures with
+-- highest variability
+
+SELECT procedure_name, 
+       procedure_id, 
+       var_score
+FROM Procedures
+ORDER BY var_score DESC 
+LIMIT 20;
