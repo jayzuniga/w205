b95d9b93321904e339ebc0af6976f57f9cbe2c7e
jayzuniga <jay_zuniga@berkeley.edu>
2017-07-07 17:32:04 -0700

    Update hospital_and_patients.sql

--- exercise_1/investigations/hospital_and_patients/hospital_and_patients.sql
+++ exercise_1/investigations/hospital_and_patients/hospital_and_patients.sql
@@ -1,1 +1,18 @@
-#1
+--
+--
+
+SELECT h.provider_id                    AS provider_id, 
+       h.hospital_name                  AS hospital_name, 
+       h.effective_care_avg_score       AS effective_care_avg_score, 
+       h.readmissions_death_avg_score   AS readmissions_death_avg_score, 
+       s.achievement_rating             AS achievement_avg_rating, 
+       s.improvement_rating             AS improvement_avg_rating, 
+       s.base_score                     AS base_score, 
+       s.consistency_score              AS consistency_score,
+       rank() over (ORDER BY h.effective_care_avg_score DESC) AS effective_care_rank,
+       rank() over (ORDER BY s.base_score DESC) AS survey_base_score_rank
+FROM Hospitals AS h
+JOIN SurveyResponses AS s
+  ON (h.provider_id = s.provider_number)
+ORDER BY effective_care_avg_score DESC
+LIMIT 10;
