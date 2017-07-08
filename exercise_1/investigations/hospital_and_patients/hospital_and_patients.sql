fd0f9c7573a8ed993502eca8e7a96b384918994c
jayzuniga <jay_zuniga@berkeley.edu>
2017-07-07 17:50:26 -0700

    Update hospital_and_patients.sql

--- exercise_1/investigations/hospital_and_patients/hospital_and_patients.sql
+++ exercise_1/investigations/hospital_and_patients/hospital_and_patients.sql
@@ -16,3 +16,23 @@ JOIN SurveyResponses AS s
   ON (h.provider_id = s.provider_number)
 ORDER BY effective_care_avg_score DESC
 LIMIT 10;
+
+
+SELECT avg(abs(t.effective_care_rank-survey_base_score_rank))      AS avg_rank_difference,
+       variance(abs(t.effective_care_rank-survey_base_score_rank)) AS var_rank_diffence
+FROM
+(
+   SELECT h.provider_id                    AS provider_id, 
+          h.effective_care_avg_score       AS effective_care_avg_score, 
+          s.base_score                     AS base_score, 
+          s.consistency_score              AS consistency_score,
+          rank() over (ORDER BY h.effective_care_avg_score DESC)
+                                           AS effective_care_rank,
+          rank() over (ORDER BY s.base_score DESC)
+                                           AS survey_base_score_rank
+FROM Hospitals AS h
+JOIN SurveyResponses AS s
+  ON (h.provider_id = s.provider_number)
+ORDER BY effective_care_avg_score DESC
+)   AS t    
+;
