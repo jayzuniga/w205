2c99a328063915dcb569d8219fe842ff5e9efa72
Jay Zuniga <jay_zuniga@berkeley.edu>
2017-08-12 11:13:25 -0700

    Update create_tweetwordcount_table.sql
    Add comments

--- exercise_2/postgres/create_tweetwordcount_table.sql
+++ exercise_2/postgres/create_tweetwordcount_table.sql
@@ -1,5 +1,7 @@
-drop table Tweetwordcount;
+-- if table already exists, drop to make sure we can refresh it
+drop table tweetwordcount;
 
-create table Tweetwordcount (
+-- create the table required for the exercise
+create table tweetwordcount (
   word         varchar(200),
   count        bigint);
