34ea67faf69225af9b7e7ed818043b2728de067f
Jay Zuniga <jay_zuniga@berkeley.edu>
2017-08-12 11:12:30 -0700

    Update create_database.sql
    Add comments

--- exercise_2/postgres/create_database.sql
+++ exercise_2/postgres/create_database.sql
@@ -1,3 +1,11 @@
+-- This file creates the database for the w205 exercise
+
+-- create the w205 user which we will use for the exercise if it
+-- doesn't already exist
 create user w205 with password 'finishex2';
-create database Tcount;
-grant all privileges on database Tcount to w205;
+
+-- create the database for the tweetwordcount app
+create database tcount;
+
+-- give w205 all the privileges it will need
+grant all privileges on database tcount to w205;
